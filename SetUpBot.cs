3a9174240c406ba6e8cca5a026d3a12ed2ea69bf
Gustavo Castellanos Alfonzo <gustavoaca1997@gmail.com>
2024-07-30 14:19:05 -0700

    Implement function

---
+++ SetUpBot.cs
@@ -0,0 +1,78 @@
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Azure.Functions.Worker.Http;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System.Globalization;
+using Telegram.Bot;
+using Newtonsoft.Json;
+using Telegram.Bot.Types.Enums;
+using System.Data;
+using System.Net;
+
+namespace ActasFunctions
+{
+    public class SetUpBot
+    {
+        private readonly ILogger<SetUpBot> _logger;
+        
+        private readonly TelegramBotClient _botClient;
+
+        public SetUpBot(ILogger<SetUpBot> logger)
+        {
+            _logger = logger;
+            _botClient = new TelegramBotClient(System.Environment.GetEnvironmentVariable("TelegramBotToken", EnvironmentVariableTarget.Process));
+        }
+
+        private const string SetUpFunctionName = "setup";
+        private const string UpdateFunctionName = "handleupdate";
+
+        [Function(SetUpFunctionName)]
+        public async Task RunAsync([HttpTrigger(AuthorizationLevel.Anonymous, "get", "post")] HttpRequestData req)
+        {
+            var handleUpdateFunctionUrl = req.Url.ToString().Replace(SetUpFunctionName, UpdateFunctionName,
+                                                ignoreCase: true, culture: CultureInfo.InvariantCulture);
+                                                Console.WriteLine($"Setting webhook at {handleUpdateFunctionUrl}");
+            await _botClient.SetWebhookAsync(handleUpdateFunctionUrl);
+        }
+
+        [Function(UpdateFunctionName)]
+        public async Task Run([HttpTrigger(AuthorizationLevel.Anonymous, "post")] HttpRequestData req)
+        {
+            var request = await req.ReadAsStringAsync();
+            var update = JsonConvert.DeserializeObject<Telegram.Bot.Types.Update>(request);
+
+            if (update.Type != UpdateType.Message)
+                return;
+            if (update.Message!.Type != MessageType.Text)
+                return;
+
+            await _botClient.SendTextMessageAsync(
+                chatId: update.Message.Chat.Id,
+                text: GetBotResponseForInput(update.Message.Text)
+            );
+        }
+
+        private string GetBotResponseForInput(string text)
+        {
+            try
+            {
+                var url = $"https://tvtcrhau2vo336qa5r66p3bygy0hazyk.lambda-url.us-east-1.on.aws/?cedula=V{text}";
+
+                var request = WebRequest.Create(url);
+                request.Method = "GET";
+
+                using var webResponse = request.GetResponse();
+                using var webStream = webResponse.GetResponseStream();
+
+                using var reader = new StreamReader(webStream);
+                dynamic obj = JsonConvert.DeserializeObject(reader.ReadToEnd());
+                return $"En el siguiente link puedes encontrar tu acta: {obj.url}";
+            }
+            catch
+            {
+                return "Por favor, dime un número de cédula válido.";
+            }
+        }
+    }
+}
